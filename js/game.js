1d2f3d8f782823aafad9069585ad679a3c4be627
Ji <zhenjun.ji@hypereal.com>
2019-06-20 14:28:57 +0900

    todo:index html

---
+++ js/game.js
@@ -0,0 +1,14 @@
+var jsStartButton = document.getElementById("startButton");
+var jsSettingButton = document.getElementById("settingButton");
+var jsExitButton = document.getElementById("exitButton");
+
+jsStartButton.onclick = function() {
+    window.location.href="html/mission.html";
+}
+
+jsSettingButton.onclick = function() {
+    window.location.href="html/setting.html";
+}
+
+jsExitButton.onclick = function() {
+}
---
+++ js/mission.js
@@ -0,0 +1,5 @@
+var jsBackButton = document.getElementById("backButton");
+
+jsBackButton.onclick = function() {
+    window.location.href="../index.html";
+}
